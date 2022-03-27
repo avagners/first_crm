6990071ebbc63c0efea4b51fd976129ec1a28649
Alexander Vagner <onemybox@gmail.com>
2022-03-27 17:52:40 +0300

    Добавлены urls и view-функции

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Главная страница
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+# Страница со списком мороженого
+def customers_list(request):
+    return HttpResponse('Список клиентов')
+
+
+# Страница с информацией об одном сорте мороженого;
+# view-функция принимает параметр pk из path()
+def customers_detail(request, pk):
+    return HttpResponse(f'Клиент номер {pk}')

bbec713224a9c3f03ee42ed57a543157a6d115ac
Alexander Vagner <onemybox@gmail.com>
2022-03-28 16:36:56 +0300

    настройка админки для модели Customer

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Customer
 
-admin.site.register(Customer)
+
+class CustomersAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'email', 'phone_number')
+    search_fields = ('last_name', 'phone_number')
+    list_filter = ('last_name',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Customer, CustomersAdmin)
--- apps/core/models.py
+++ apps/core/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 
 class Customer(models.Model):
-    first_name = models.TextField(max_length=150)
-    last_name = models.TextField(max_length=150)
-    email = models.EmailField(max_length=254, blank=True)
-    phone_number = models.TextField(max_length=15)
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия'
+    )
+    email = models.EmailField(max_length=254, blank=True, unique=True)
+    phone_number = models.CharField(
+        max_length=15,
+        verbose_name='Телефон',
+        unique=True
+    )

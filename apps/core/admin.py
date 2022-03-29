e690ced3e39da7bf3dab74a8fc1cd7b9c196a1bf
Alexander Vagner <onemybox@gmail.com>
2022-03-29 16:03:40 +0300

    add models Order, Product

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Customer
+from .models import Customer, Product, Order
 
 
 class CustomersAdmin(admin.ModelAdmin):
@@ -10,4 +10,22 @@ class CustomersAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name_product', 'price', 'description')
+    search_fields = ('name_product',)
+    list_filter = ('name_product',)
+    empty_value_display = '-пусто-'
+
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'pay_date', 'customer', 'product', 'total_cost', 'comments'
+    )
+    search_fields = ('customer', 'product')
+    list_filter = ('pay_date',)
+    empty_value_display = '-пусто-'
+
+
 admin.site.register(Customer, CustomersAdmin)
+admin.site.register(Product, ProductsAdmin)
+admin.site.register(Order, OrdersAdmin)

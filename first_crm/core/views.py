5b4951df1f17d718db767fad1636e73b164fc39a
Alexander Vagner <onemybox@gmail.com>
2022-03-30 17:03:17 +0300

    перенес index.html в core, fixed links in base.html

--- first_crm/core/views.py
+++ first_crm/core/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    template = 'customers/index.html'
-    return render(request, template)
+    template = 'core/index.html'
+    title = 'Первая CRM - Главная страница'
+    context = {
+        'title': title,
+        'text': 'Главная страница',
+    }
+    return render(request, template, context)

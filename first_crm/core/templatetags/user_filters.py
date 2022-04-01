a8a96268347111b7abf33e8176ea9f5755acc3e1
Alexander Vagner <onemybox@gmail.com>
2022-04-01 17:13:07 +0300

    удалил лишний фильтр

--- first_crm/core/templatetags/user_filters.py
+++ first_crm/core/templatetags/user_filters.py
@@ -6,8 +6,3 @@ register = template.Library()
 @register.filter
 def addclass(field, css):
     return field.as_widget(attrs={'class': css})
-
-
-@register.filter
-def addplaceholder(field, css):
-    return field.as_widget(attrs={'placeholder': css})

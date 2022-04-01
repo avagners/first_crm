d0b4991585c51b9dedaa6aee1fe027b66acafd36
Alexander Vagner <onemybox@gmail.com>
2022-04-01 17:22:35 +0300

    Customers: доработал форму

--- first_crm/customers/forms.py
+++ first_crm/customers/forms.py
@@ -6,3 +6,8 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ('last_name', 'first_name', 'email', 'phone_number')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'placeholder': field})

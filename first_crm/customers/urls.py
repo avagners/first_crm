63023ac9bcb0a9f8b0dbef44540c01c11b88507e
Alexander Vagner <onemybox@gmail.com>
2022-04-03 17:12:45 +0300

    Customers: форма редактирования клиента

--- first_crm/customers/urls.py
+++ first_crm/customers/urls.py
@@ -6,7 +6,8 @@ app_name = 'customers'
 urlpatterns = [
     path('', views.customers_list, name='customers_list'),
     path('<int:pk>/', views.customers_detail,
-         name='customers_detail'
-         ),
-    path('new_customer/', views.NewCustomerView.as_view(), name='new_customer')
+         name='customers_detail'),
+    path('new_customer/', views.NewCustomerView.as_view(),
+         name='new_customer'),
+    path('<int:pk>/edit/', views.customer_edit, name='customer_edit'),
 ]
--- first_crm/customers/views.py
+++ first_crm/customers/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.shortcuts import render
 from .models import Customer
 from .forms import CustomerForm
+from django.shortcuts import get_object_or_404, redirect
 
 
 def customers_list(request):
@@ -26,7 +27,23 @@ def customers_detail(request, pk):
     return render(request, template, context)
 
 
+def customer_edit(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    form = CustomerForm(
+        request.POST or None,
+        files=request.FILES or None,
+        instance=customer
+    )
+    if form.is_valid():
+        form.save()
+        return redirect('customers:customers_list')
+    context = {
+        'form': form,
+        'customer': customer}
+    return render(request, 'customers/customer_form.html', context)
+
+
 class NewCustomerView(CreateView):
     form_class = CustomerForm
-    template_name = 'customers/new_customer_form.html'
+    template_name = 'customers/customer_form.html'
     success_url = reverse_lazy('customers:customers_list')

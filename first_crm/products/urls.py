e6ba51659d9425df5d12c2eebb2b048e12b3ecae
Alexander Vagner <onemybox@gmail.com>
2022-04-05 18:02:04 +0300

    Products: механизм загрузки услуг из csv файла #5

--- first_crm/products/urls.py
+++ first_crm/products/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<int:pk>/', views.products_detail, name='products_detail'),
     path('new_product/', views.NewProductView.as_view(), name='new_product'),
     path('<int:pk>/edit/', views.product_edit, name='product_edit'),
+    path('upload_file/', views.upload_file, name='upload_file'),
 ]

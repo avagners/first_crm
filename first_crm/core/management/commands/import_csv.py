306fa0b0767df6578af1a6f2cfb918f862ef3247
Alexander Vagner <onemybox@gmail.com>
2022-04-05 14:48:26 +0300

    правки команды import_csv и файла products.csv

--- first_crm/core/management/commands/import_csv.py
+++ first_crm/core/management/commands/import_csv.py
@@ -10,18 +10,17 @@ class Command(BaseCommand):
     которые располагаются в директории /static/data/
     """
     def handle(self, *args, **options):
-        self.import_titles()
+        self.import_products()
         print('Загрузка тестовых данных завершена.')
 
-    def import_titles(self, file='products.csv'):
+    def import_products(self, file='products.csv'):
         print(f'Загрузка {file}...')
         file_path = f'static/data/{file}'
         with open(file_path, newline='', encoding='utf-8') as f:
             reader = csv.DictReader(f)
             for row in reader:
-                print(reader)
                 status, created = Product.objects.update_or_create(
-                    name_product=row['name_product'],
-                    description=row['description'],
-                    price=row['price']
+                    name_product=row['Наименование услуги'],
+                    description=row['Описание'],
+                    price=row['Цена']
                 )
